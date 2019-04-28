c1296d122147f7c0432734e5ad1d1b628c44b657
Luis Enrique Neri <A01745995@itesm.mx>
2019-04-28 01:18:20 -0500

    hola

---
+++ app/src/main/java/com/brodasistah/mx/hmx2/MainScreen.java
@@ -0,0 +1,26 @@
+package com.brodasistah.mx.hmx2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.SurfaceView;
+import android.widget.TextView;
+import com.google.android.gms.vision.CameraSource;
+
+import org.w3c.dom.Text;
+
+
+public class MainScreen extends AppCompatActivity {
+    SurfaceView cameraView;
+    TextView textView;
+    CameraSource cameraSource;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_item);
+
+        cameraView=(SurfaceView)findViewById(R.id.surface_view);
+        textView=(TextView)findViewById(R.id.text_view);
+    }
+}
---
+++ app/src/main/java/com/brodasistah/mx/hmx2/Visor.java
@@ -0,0 +1,5 @@
+package com.brodasistah.mx.hmx2;
+
+public class Visor {
+
+}
